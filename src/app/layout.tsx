37303d013bd2448e6bc52b8218e0e27ae72b011c
mdimado <mdimad005@gmail.com>
2024-12-20 18:51:01 +0530

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Minutes of Meeting Application",
-  description: "Create and manage meeting minutes",
+  title: "COSC Minutes of the meet",
+  description: "Create and manage MoMs",
 };
 
 export default function RootLayout({
